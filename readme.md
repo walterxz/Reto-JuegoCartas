399f8c90ba506ca5a1833bf47e8258e0cc17551a
Walter Miguel Diaz Jaramillo <waltz.mx@gmail.com>
2020-08-11 08:37:03 -0500

    Start project

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Reto juego de Cartas 3 y 2
+
+Video referencia:
+https://www.youtube.com/watch?v=z5qaucfJasw&app=desktop
---
+++ scripts/gameLogic.js
@@ -0,0 +1,38 @@
+document.getElementById("start").addEventListener("click", startGame);
+
+let cards = [];
+let pcCards=[]
+let myCards=[]
+let trash=[]
+let selectedCard=''
+let cardTaken=false
+
+
+function startGame() {
+  console.log("Barajeando Cartas");
+  //Inicializar  variables
+
+}
+
+function takeCard(){
+ //sacar carta del arreglo
+ //la coloca en selectedCard
+}
+
+function changeCard(){
+ //actualizar card Taken
+ //actualizar cartas tomadas
+}
+
+function winValidation(){
+    if(true){
+    //Si se repiten 3 y 2 cartas Mostrar mensaje de Win
+    }
+}
+
+function ia (){
+    // Si no tiene carta repetida cambiar esa
+    //No hacer cambio de una si tiene 3 repetidas
+}
+
+
